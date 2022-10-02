8564bbcd10d720ca505556909c24926929e8b2e5
Aaron Di <aarondi1119@gmail.com>
2022-10-02 19:39:11 +0800

    ✨ feat(Login): Finish Login CSS and router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/home/Home.vue'
+import Login from '../views/login/Login.vue'
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home'
-  // }
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
   // {
   //   path: '/about',
   //   name: 'About',
