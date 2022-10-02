8988caf271ceac6f159bed3c300af3107d60888b
Aaron Di <aarondi1119@gmail.com>
2022-10-02 15:47:56 +0800

    🦄 refactor(Home): Split into components

---
+++ src/views/home/Docker.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="docker">
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe750;</div>
+      <div class="docker__item__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe73e;</div>
+      <div class="docker__item__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe737;</div>
+      <div class="docker__item__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe75e;</div>
+      <div class="docker__item__title">我的</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Docker'
+}
+</script>
+
+<style lang="scss">
+@import '../../style/variables.scss';
+@import '../../style/mixins.scss';
+
+.docker {
+  display: flex;
+  box-sizing: border-box;
+  position: absolute;
+  padding: 0 .18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: .49rem;
+  border-top: 1px solid $content-bgColor;
+  color: $content-fontColor;
+
+  &__item {
+    flex: 1;
+    text-align: center;
+
+    .iconfont {
+      margin: .07rem 0 .02rem 0;
+      font-size: .18rem;
+    }
+
+    &--active {
+      color: #1FA4FC;
+    }
+
+    &__title {
+      font-size: .2rem;
+      transform: scale(.5, .5);
+      transform-origin: center top;
+    }
+  }
+}
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+    <StaticPart />
+    <NearBy />
+  </div>
+  <Docker />
+</template>
+
+<script>
+import StaticPart from './StaticPart.vue'
+import NearBy from './Nearby.vue'
+import Docker from './Docker.vue'
+export default {
+  name: 'Home',
+  components: { StaticPart, NearBy, Docker }
+}
+</script>
+
+<style lang="scss">
+.wrapper {
+  overflow-y: auto;
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: .5rem;
+  right: 0;
+  padding: 0 .18rem .1rem .18rem;
+}
+
+</style>
