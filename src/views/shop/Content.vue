f7c03005cd5f7fb617978ea4d236e250006e48e6
Aaron Di <aarondi1119@gmail.com>
2022-10-04 00:30:35 +0800

    ✨ feat(Shop-Cart): Synchronize cart and list

--- src/views/shop/Content.vue
+++ src/views/shop/Content.vue
@@ -28,8 +28,8 @@
 <script>
 import { reactive, ref, toRefs, watchEffect } from 'vue'
 import { useRoute } from 'vue-router'
-import { useStore } from 'vuex'
 import { get } from '../../utils/request'
+import { useCommonCartEffect } from './commonCartEffect'
 
 const categories = [
   { name: '全部商品', tab: 'all' },
@@ -65,18 +65,6 @@ const useCurrentListEffect = (currentTab, shopId) => {
   return { list }
 }
 
-// 购物车相关逻辑
-const useCartEffect = () => {
-  const store = useStore()
-  const { cartList } = toRefs(store.state)
-  const changeCartItemInfo = (shopId, productId, productInfo, num) => {
-    store.commit('changeCartItemInfo', {
-      shopId, productId, productInfo, num
-    })
-  }
-  return { cartList, changeCartItemInfo }
-}
-
 export default {
   name: 'Content',
   setup () {
@@ -84,7 +72,7 @@ export default {
     const shopId = route.params.id
     const { currentTab, handleTabClick } = useTabEffect()
     const { list } = useCurrentListEffect(currentTab, shopId)
-    const { cartList, changeCartItemInfo } = useCartEffect()
+    const { cartList, changeCartItemInfo } = useCommonCartEffect()
     return { categories, currentTab, list, handleTabClick, cartList, shopId, changeCartItemInfo }
   }
 }
