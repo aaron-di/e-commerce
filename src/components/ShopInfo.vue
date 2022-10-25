abbb79bd42c057b2a1e81bbabf15bd70ab5165a3
Aaron Di <aarondi1119@gmail.com>
2022-10-25 19:36:49 +0800

    ✨ feat(SearchList): Finish SearchList

--- src/components/ShopInfo.vue
+++ src/components/ShopInfo.vue
@@ -9,6 +9,16 @@
         <span class="shop__content__tag">基础运费¥{{ item.expressPrice }}</span>
       </div>
       <p class="shop__content__highlight">{{ item.slogan }}</p>
+      <div v-if="item.products" class="shop__products">
+        <div v-for="product in item.products" :key="product.name" class="shop__product">
+          <img :src="product.imgUrl" class="shop__product__img" />
+          <p class="shop__product__title">{{product.name}}</p>
+          <p class="shop__product__price">
+            <span class="yen">&yen;</span><span class="price">{{product.price}}</span>
+            <span class="origin">&yen;{{product.price}}</span>
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -62,6 +72,45 @@ export default {
       line-height: .18rem;
       font-size: .13rem;
       color: $highlight-fontColor;
+    }
+  }
+  &__products {
+    overflow: hidden;
+    margin: .08rem .07rem 0 -.18rem;
+  }
+  &__product {
+    width: 33.33%;
+    padding-left: .18rem;
+    box-sizing: border-box;
+    float: left;
+    &__img {
+      width: 100%;
+    }
+    &__title {
+      margin: .04rem 0 0 0;
+      line-height: .17rem;
+      font-size: .12rem;
+      color: #333;
+      @include ellipsis;
+    }
+    &__price {
+      line-height: .2rem;
+      margin: .02rem 0 0 0;
+      color: #999;
+      font-size: .14rem;
+      @include ellipsis;
+      .yen {
+        font-size: .12rem;
+        color: #E93B3B;
+      }
+      .price {
+        color: #E93B3B;
+      }
+      .origin {
+        margin-left: .06rem;
+        font-size: .12rem;
+        text-decoration: line-through;
+      }
     }
   }
 }
---
+++ src/views/searchList/SearchList.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="wrapper">
+    <div class="search">
+      <div class="search__back iconfont" @click="handleBackClick">&#xe779;</div>
+      <div class="search__content">
+        <span class="search__content__icon iconfont">&#xe752;</span>
+        <input
+          class="search__content__input"
+          placeholder="请输入商品名称"
+          v-model="keyword"
+          @change="handleSearchInputChange"/>
+      </div>
+    </div>
+    <router-link
+      v-for="item in searchList"
+      :key="item._id"
+      :to="`/shop/${item._id}`"
+    >
+      <ShopInfo :item="item" />
+    </router-link>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { get } from '../../utils/request'
+import { useRoute, useRouter } from 'vue-router'
+import ShopInfo from '../../components/ShopInfo'
+
+// 点击回退逻辑
+const useBackRouterEffect = () => {
+  const router = useRouter()
+  const handleBackClick = () => {
+    router.back()
+  }
+  return handleBackClick
+}
+
+// 搜索列表相关逻辑
+const useSearchListEffect = () => {
+  const searchList = ref([])
+  const getSearchList = async (keyword) => {
+    const result = await get('/api/shop/search', { keyword })
+    if (result?.errno === 0 && result?.data?.length) {
+      searchList.value = result.data
+    }
+  }
+  return { searchList, getSearchList }
+}
+
+export default {
+  name: 'SearchList',
+  components: { ShopInfo },
+  setup () {
+    const route = useRoute()
+    // 搜索关键词逻辑
+    const keyword = ref(route.query.keyword || '')
+    const handleSearchInputChange = () => {
+      getSearchList(keyword.value)
+    }
+    const handleBackClick = useBackRouterEffect()
+    // 获取搜索列表
+    const { searchList, getSearchList } = useSearchListEffect()
+    getSearchList(keyword.value)
+
+    return {
+      keyword,
+      searchList,
+      getSearchList,
+      handleBackClick,
+      handleSearchInputChange
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../style/variables.scss';
+
+.wrapper {
+  padding: 0 .18rem;
+
+  a {
+    text-decoration: none;
+  }
+}
+
+.search {
+  display: flex;
+  margin: .14rem 0 .04rem 0;
+  line-height: .32rem;
+
+  &__back {
+    width: .3rem;
+    font-size: .2rem;
+    color: #B6B6B6;
+  }
+
+  &__content {
+    display: flex;
+    flex: 1;
+    background: $search-bgColor;
+    border-radius: .16rem;
+
+    &__icon {
+      width: .44rem;
+      text-align: center;
+      color: $search-fontColor;
+    }
+
+    &__input {
+      display: block;
+      width: 100%;
+      padding-right: .2rem;
+      border: none;
+      outline: none;
+      background: none;
+      height: .32rem;
+      font-size: .14rem;
+      color: $content-fontColor;
+
+      &::placeholder {
+        color: $content-fontColor;
+      }
+    }
+  }
+}
+</style>
